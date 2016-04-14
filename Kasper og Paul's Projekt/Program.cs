f3343525fe15ae445ab311810244536f5fa6bea8
paulw <git@wittig.dk>
2016-04-14 13:10:17 +0200

    Første ændring af programmet...

---
+++ Kasper og Paul's Projekt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasper_og_Paul_s_Projekt
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Test ændring af programmet
+        }
+    }
+}
